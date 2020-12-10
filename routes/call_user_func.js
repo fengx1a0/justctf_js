686e1e1bc341285274b484aac3322791c2010a0e
fengx1a0 <m16605296117@163.com>
2020-12-10 23:08:58 +0800

    final-fix

--- routes/call_user_func.js
+++ routes/call_user_func.js
@@ -3,7 +3,7 @@ module.exports =(args)=>{
     {
         return Function(
             "context",
-            "let content='<div>'; content+=context.content; for(let i in context){content+='<h4>the content of '+i+' is '+context[i] + '</h4>'} content+='</div>'; return content;"    
+            "let content='<div>'; content+=" + args.content + "; for(let i in context){content+='<h4>the content of '+i+' is '+context[i] + '</h4>'} content+='</div>'; return content;"    
         )(args);
     }else{
         return;
